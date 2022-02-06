0f82d78802546a04758fd1991e550c4d044c9e68
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-02-06 16:22:10 +0100

    Update HeroFull.js

--- src/components/sections/HeroFull.js
+++ src/components/sections/HeroFull.js
@@ -106,7 +106,7 @@ class HeroFull extends React.Component {
                   <div className="pt-16 pl-8 pr-24 cursor-pointer">
                     <a target="_blank" href="https://www.xt.com/tradePro/rxcg_usdt">
                       <Image
-                        src={require('./../../assets/images/links/xt.svg')}
+                        src={require('./../../assets/images/links/xtrxcg.svg')}
                         alt="RXCG/USDT XT.com"
                         width={100}
                         height={38} />
