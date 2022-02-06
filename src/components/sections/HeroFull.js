26e64529b89819b6cc6a918f66ce9edd9a44daae
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-02-07 00:55:08 +0100

    Update HeroFull.js

--- src/components/sections/HeroFull.js
+++ src/components/sections/HeroFull.js
@@ -66,12 +66,12 @@ class HeroFull extends React.Component {
                 </div>
               </div>
                   <div className="pt-16 pl-8 pr-24 cursor-pointer">
-                    <a target="_blank" href="https://t.me/rxcgames">
+                    <a target="_blank" href="https://coinmarketcap.com/currencies/rxcgames/">
                       <Image
-                        src={require('./../../assets/images/links/telegram.svg')}
-                        alt="RXCG Telegram"
-                        width={38}
-                        height={38} />
+                        src={require('./../../assets/images/links/CoinMarketCap.svg')}
+                        alt="Coinmarketcap RXCG"
+                        width={58}
+                        height={58} />
                     </a>
                   </div>
                   <div className="pt-16 pl-8 pr-24 cursor-pointer">
