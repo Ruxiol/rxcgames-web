4471ade041653ac72b7487d76d7abd63212a5e7d
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-06-23 16:20:40 +0200

    Update FooterSocial.js

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -34,8 +34,8 @@ const FooterSocial = ({
               <Image
                 src={require('./../../../assets/images/chainplay-logo.svg')}
                 alt="RXCG Chainplay"
-                width={38}
-                height={38} />
+                width={48}
+                height={48} />
             </a>
           </div>
           <div className="pt-16 pl-8 pr-24 cursor-pointer">
