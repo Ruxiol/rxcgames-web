5f0367739e5a8e3707078555d570bb04f8f93779
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-06-23 16:32:33 +0200

    Update FooterSocial.js

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -25,44 +25,8 @@ const FooterSocial = ({
               <Image
                 src={require('./../../../assets/images/chainplay-web.svg')}
                 alt="RXCG Chainplay"
-                width={38}
-                height={38} />
-            </a>
-          </div>
-          <div className="pt-16 pl-8 pr-24 cursor-pointer">
-            <a target="_blank" href="https://t.me/rxcgames">
-              <Image
-                src={require('./../../../assets/images/links/telegram.svg')}
-                alt="RXCG Telegram"
-                width={38}
-                height={38} />
-            </a>
-          </div>
-          <div className="pt-16 pl-8 pr-24 cursor-pointer">
-            <a target="_blank" href="https://twitter.com/rxc_games">
-              <Image
-                src={require('./../../../assets/images/links/twitter.svg')}
-                alt="RXCG Twitter"
-                width={38}
-                height={38} />
-            </a>
-          </div>
-          <div className="pt-16 pl-8 pr-24 cursor-pointer">
-            <a target="_blank" href="https://pancakeswap.finance/swap?outputCurrency=0x7c59a57fc16eac270421b74615c4bc009ecd486d">
-              <Image
-                src={require('./../../../assets/images/links/pcs.svg')}
-                alt="RXCG PCS"
-                width={38}
-                height={38} />
-            </a>
-          </div>
-          <div className="pt-16 pl-8 pr-24 cursor-pointer">
-            <a target="_blank" href="https://poocoin.app/tokens/0x7c59a57fc16eac270421b74615c4bc009ecd486d">
-              <Image
-                src={require('./../../../assets/images/links/poocoin.svg')}
-                alt="RXCG Poocoin"
-                width={38}
-                height={38} />
+                width={338}
+                height={138} />
             </a>
           </div>
         </div>
