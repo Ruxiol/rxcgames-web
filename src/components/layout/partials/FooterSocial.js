11a41c344fde31c91b5c2ccb95153c9faf546f60
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-06-23 17:17:52 +0200

    Update FooterSocial.js

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -22,6 +22,7 @@ const FooterSocial = ({
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <div className="pt-16 pl-8 pr-24 cursor-pointer">
             <a target="_blank" href="https://chainplay.gg/games/rxc-games/">
+              Chainplay
               <Image
                 src={require('./../../../assets/images/chainplay-logo.svg')}
                 alt="RXCG Chainplay"
