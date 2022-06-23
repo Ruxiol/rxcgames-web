954555abbb92aa62f5cf7010a60d334830fadf9d
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-06-23 09:21:17 +0200

    Update HeroFull.js

--- src/components/sections/HeroFull.js
+++ src/components/sections/HeroFull.js
@@ -131,7 +131,7 @@ class HeroFull extends React.Component {
             </p>              
               <Image
                 className="has-shadow"
-                src={require('./../../assets/images/hero-image.png')}
+                src={require('./../../assets/images/1.png')}
                 alt="RXCG Platform"
                 width={896}
                 height={504} />
