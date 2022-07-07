0750f2f074175c642a640f6427ac47b3e40c389e
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-07-07 19:19:02 +0200

    Update HeroFull.js

--- src/components/sections/HeroFull.js
+++ src/components/sections/HeroFull.js
@@ -67,8 +67,8 @@ class HeroFull extends React.Component {
                   <Button className="btn-play" tag="a" color="primary" wideMobile href="https://stake.rxcgames.com/">
                     Stake
                   </Button>
-                  <Button className="btn-play" tag="a" color="primary" wideMobile href="https://rxcgames.com/">
-                    Lottery
+                  <Button className="btn-play" tag="a" color="primary" wideMobile href="https://cloud.rxcgames.com/">
+                    Cloud
                   </Button>
                   <Button className="btn-play" tag="a" color="primary" wideMobile href="https://swap.rxcgames.com/">
                     Swap
