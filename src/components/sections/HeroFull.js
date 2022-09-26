cd16d8a8c2cc20c915207a39c91584fafc0267aa
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2022-09-26 19:09:49 +0200

    Update HeroFull.js

--- src/components/sections/HeroFull.js
+++ src/components/sections/HeroFull.js
@@ -129,6 +129,12 @@ class HeroFull extends React.Component {
                 alt="RXCG Casino"
                 width={500}
                 height={504} />
+              Poker Module
+              <Image
+                src={require('./../../assets/images/3.jpg')}
+                alt="RXCG Poker"
+                width={450}
+                height={504} />                  
             </div>
           </div>
         </div>
