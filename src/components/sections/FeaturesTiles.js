ada1a8b02af77c8eb2874503ec93d4e587ecd893
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2023-01-22 16:56:07 +0100

    Update FeaturesTiles.js

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -93,8 +93,8 @@ class FeaturesTiles extends React.Component {
                       <Image
                         src={require('./../../assets/images/moneybag.svg')}
                         alt="Play and Earn"
-                        width={40}
-                        height={40} />
+                        width={48}
+                        height={48} />
                     </div>
                   </div>
                   <div className="features-tiles-item-content">
