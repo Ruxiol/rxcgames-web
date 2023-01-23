f9ca00ee2fca91a1fe94e60b042409991af0fecf
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2023-01-23 18:39:24 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Logo from './partials/Logo';
 import FooterNav from './partials/FooterNav';
+import FooterSocial from './partials/FooterSocial';
 
 const propTypes = {
   topOuterDivider: PropTypes.bool,
